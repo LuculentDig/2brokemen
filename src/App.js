56a859668cdded4ed8274c80ad8dffd004905ac8
Wenyuan Wu <wenyuanw@utexas.edu>
2019-06-26 21:55:45 -0500

    adding pages but not referencing them yet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './pages/Navigation'
+import Home from './pages/home'
 
 function App() {
   return (
     <div>
       TEsing different stuff
-      
+
     </div>
   );
 }
